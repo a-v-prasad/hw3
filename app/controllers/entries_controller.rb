fbc16a1457df7f2f9d7b2b841670c18e4cd60148
a-v-prasad <alisha.prasad@kellogg.northwestern.edu>
2026-02-25 02:28:50 +0000

    Updated Entries_controller + added new.html.erb for entries - added ability to add activies under a place

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -1,2 +1,20 @@
 class EntriesController < ApplicationController
+
+  # GET /entries/new
+  def new
+    @place_id = params["place_id"]
+  end
+
+  # POST /entries
+  def create
+    @entry = Entry.new
+    @entry["title"] = params["title"]
+    @entry["description"] = params["description"]
+    @entry["occurred_on"] = params["occurred_on"]
+    @entry["place_id"] = params["place_id"]
+    @entry.save
+
+    redirect_to "/places/#{@entry["place_id"]}"
+  end
+
 end
